69fce9b40b1f798a659ff21f7289c4a3e8e7acab
Lino873 <linotulian271@gmail.com>
2026-08-25 22:08:17 -0300

    clase 5

---
+++ clase 5/practica.mjs
@@ -0,0 +1,34 @@
+const numeros = [1, 2, 3, 4, 5, 6]
+//function recorrer() {}
+
+//const recorrer = () => {
+//funcion flech bus
+
+//numeros.map(recorrer)
+// const nuevoarreglo = numeros.map((numero)=> {
+//   console.log(numero)
+//   return numero
+// })
+// trabajar con un objeto
+const productos = [
+    {
+        nombre:"pantalon",
+        precio: 100
+    }
+    ,
+    {
+        nombre:"remera",
+        precio: 50
+    }
+]
+
+const prodctosconinteres = productos.map((producto) =>{
+    const productocambiado = {
+        nombre: producto.nombre,
+        nombre: producto.precio * 1.1
+
+    }
+    return productocambiado
+
+})
+console.log(prodctosconinteres)
